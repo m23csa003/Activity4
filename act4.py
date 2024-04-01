24220626e7a558d9ef1a312b5827a16df5c8140a
KhadgaA <khadgajyothalli@gmail.com>
2024-04-01 13:04:18 +0530

    calc d, successful rollforward

--- act4.py
+++ act4.py
@@ -4,8 +4,10 @@ def func1(a, b):
 
 
 def func2(a, b):
-    c = func1(a, b)
-    return a - b, a, b, c
+    c,*_ = func1(a, b)
+    print(f"c: {c}")
+    d = c + a
+    return a - b, a, b, c,d
 
 
 if __name__ == "__main__":
